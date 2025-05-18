183bb79efe0cc9cff6df0eb139f1fcfcdede83a5
badien <Vubadien2172004@gmail.com>
2025-05-18 15:45:59 +0700

    up

--- src/routes/handler/home_handler.rs
+++ src/routes/handler/home_handler.rs
@@ -1,13 +1,13 @@
-use actix_web::{get, web, Responder};
+use actix_web::{get, post, web, Responder};
 use crate::utils::api_response;
 
 #[get("/hello/{name}")]
 async fn greet(name: web::Path<String>) -> impl Responder {
-    format!("Hello {name}!")
+     api_response::ApiResponse::new(200, "test".to_string())
 }
+
 
 #[get("/test")]
 async fn test() -> impl Responder {
     api_response::ApiResponse::new(200, "test".to_string())
 }
-
